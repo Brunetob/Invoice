794795a0f3722193bbf3a532a858fbe0ae4fd6ad
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-12-18 08:14:48 -0500

    push de prueba

--- src/main/kotlin/com/example/factura/Controller/ClientController.kt
+++ src/main/kotlin/com/example/factura/Controller/ClientController.kt
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/api/clients")
 class ClientController {
     @Autowired
     lateinit var clientService: ClientService
