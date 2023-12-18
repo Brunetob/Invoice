e9fdd649d207e375f782492df4d36e1bce2aadd2
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-12-18 10:22:49 -0500

    alterar una tabla para agregarle un campo (email) en tabla cliente

--- src/main/kotlin/com/example/factura/Controller/ClientController.kt
+++ src/main/kotlin/com/example/factura/Controller/ClientController.kt
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/clients")
+@RequestMapping("/client")
 class ClientController {
     @Autowired
     lateinit var clientService: ClientService
---
+++ src/main/resources/db/migration/V3__add-email.sql
@@ -0,0 +1,3 @@
+-- Agregar un nuevo campo (email) a la tabla cliente
+ALTER TABLE client
+    ADD COLUMN email VARCHAR(120);
